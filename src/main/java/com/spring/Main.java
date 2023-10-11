599f5f6a2399a24382a8c9063ca22af4a9308b57
pandeysankur <mailme.pandey19@gmail.com>
2023-10-11 11:45:16 +0530

    Implement the Docker File

---
+++ src/main/java/com/spring/Main.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/config.xml");
+       Student student1=(Student) context.getBean("student1");
+        System.out.println(student1);
+    }
+}
